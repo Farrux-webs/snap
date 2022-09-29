77e8a40507757693acd57af331367ce7e7db7c96
RavFarrukh <ravshanbekovf02@gmail.com>
2022-09-29 10:06:31 -0700

    hamburger menu qoshldi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const elfirstdropdown = document.querySelector(".first-dropdown");
 const elfirstdropdowninline = document.querySelector(".dropdown");
+const elbody = document.querySelector("body");
+
 
 elfirstdropdown.addEventListener("click", () => {
     if(elfirstdropdowninline.style.display == "none") {
@@ -9,6 +11,7 @@ elfirstdropdown.addEventListener("click", () => {
     }
     
 })
+
 
 
 const elSecondDropdown = document.querySelector(".second-dropdown");
@@ -21,3 +24,28 @@ elSecondDropdown.addEventListener("click", () => {
         elSecondDropdowninline.style.display = "none";
     }
 })
+
+
+const elNavbar = document.querySelector(".navbar");
+const elhamburger = document.querySelector(".hamburger");
+
+elhamburger.addEventListener("click", () => {
+    if(elNavbar.style.transform == "translate(100%)"){
+        elNavbar.style.transform = "translate(0)";
+        elhamburger.classList.remove("bi-list");
+        elhamburger.classList.add("bi-x-lg");
+        elNavbar.style.boxShadow = "0px 0px 700px 100px black"
+    }else{
+        elNavbar.style.transform = "translate(100%)";
+        elhamburger.classList.remove("bi-x-lg");
+        elhamburger.classList.add("bi-list");
+        elNavbar.style.boxShadow = "none"
+    }
+    
+})
+
+
+
+
+
+
